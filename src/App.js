171c05b3ce676b90f57c51abda574559b8d0f99f
shibatadaiki <shibatadaiki92@gmail.com>
2019-03-24 19:27:31 +0900

    express add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
+// Reactコンポーネントの基本的な機能を「Component」に代入している？
 import React, { Component } from 'react';
+
+// ./logo.svgファイルの画像データを読み込んで定数？「logo」に代入している
 import logo from './logo.svg';
+// './App.css'は、同じsrcディレクトリ内にあるApp.cssファイルをインストールしている
 import './App.css';
 
+// Reactコンポーネントの基本的な機能を継承している？
+// extendsでクラス継承する
 class App extends Component {
   render() {
+    // return React.createElement('div', null, 'h1', 'my name is daiki shibata')
+
     return (
+      // これらのDOM要素をreturnして呼び出し元に返す
+      // {logo}で、./logo.svgのデータ内容を出力する
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <p>Hello React!!!!!!!!!!!</p>
+          {/* jsxのコメントアウトはこのようにする */}
+          {/* jsxはHTMLのように見えるが、実際はHTMLのように書けるJavaScript */}
+          {/* DOMになる際にJSによって変換される。シンタックスシュガー */}
+          {/*
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          */}
         </header>
       </div>
     );
   }
 }
 
+// 「export default App」で、Appクラスの値を、このファイルを読み込んだ際の返り値として返す
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+// 'react' 'react-dom'はreactライブラリ（ライブラリをインストールしている）
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// './index.css'は、同じsrcディレクトリ内にあるindex.cssファイルをインストールしている
 import './index.css';
+
+// './App'は、同じsrcディレクトリ内にあるApp.jsファイルのclass Appのオブジェクトをインストールしている
+// App.jsファイルにあるインストールされたclass Appのオブジェクトが、このファイル（index.jsファイル）の「App」という
+// 定数（だっけ？）に代入されている。
 import App from './App';
+
+// ./serviceWorker.jsのファイルでexport（出力）されている全ての内容（「*」）を読み込み、
+// それを「serviceWorker」という名前から使用できるようにしている
+// この場合では、「function unregister()」と「function register(config)」メソッドが
+// 「serviceWorker」から使用できるようになっている。
 import * as serviceWorker from './serviceWorker';
 
+// この<App />の「App」が、App.jsファイルのclass Appのオブジェクトであり、
+// それが ReactDOM.render されることで、 class App の render() で return されているDOM要素の値が
+// <App />に代入される。
+// さらに、第二引数のdocument.getElementById('root')で、index.htmlのid=rootの子要素にrenderされたDOM要素が挿入される
+
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<p>Test!!!!!!!!!!</p>, document.getElementById('root'));
 
 //アプリをオフラインで動作させて読み込みを速くしたい場合は、変更できます。
 //下のregister（）に登録解除します。これにはいくつかの落とし穴があります。
 //サービスワーカーの詳細：https://bit.ly/CRA-PWA
+
+// 「function unregister()」と「function register(config)」メソッドが
+// 「serviceWorker」から使用できるようになっている。
 serviceWorker.unregister();
